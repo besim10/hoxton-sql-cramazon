d12343997ac04061cba335f3a4f8795bd84140fe
Besim <besimsokoli10@gmail.com>
2022-03-10 16:59:34 +0100

    added endpoints for individual item and user

--- index.ts
+++ index.ts
@@ -15,11 +15,58 @@ app.get("/users", async (req, res) => {
   });
   res.send(users);
 });
+
+app.get("/users/:email", async (req, res) => {
+  const email = req.params.email;
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: { email: email },
+      include: {
+        orders: {
+          select: { item: true, quantity: true },
+        },
+      },
+    });
+
+    if (user) {
+      res.send(user);
+    } else {
+      res.status(404).send({ error: "User not found." });
+    }
+  } catch (err) {
+    // @ts-ignore
+    res.status(400).send({ error: err.message });
+  }
+});
 app.get("/items", async (req, res) => {
   const items = await prisma.item.findMany({
     include: { orders: { select: { quantity: true, user: true } } },
   });
   res.send(items);
+});
+app.get("/items/:title", async (req, res) => {
+  const title = req.params.title;
+
+  try {
+    const item = await prisma.item.findUnique({
+      where: { title },
+      include: {
+        orders: {
+          select: { user: true, quantity: true },
+        },
+      },
+    });
+
+    if (item) {
+      res.send(item);
+    } else {
+      res.status(404).send({ error: "item not found." });
+    }
+  } catch (err) {
+    // @ts-ignore
+    res.status(400).send({ error: err.message });
+  }
 });
 app.listen(PORT, () => {
   console.log(`Server up and running: http://localhost:${PORT}`);
